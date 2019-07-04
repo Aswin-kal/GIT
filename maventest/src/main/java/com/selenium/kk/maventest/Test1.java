e61ff2d4e09ab59cedeebddafcf271e8a32fc3be
ITsupport <ITsupport@RDT102.mlad.in>
2019-07-04 16:41:20 +0530

    First commit

---
+++ maventest/src/main/java/com/selenium/kk/maventest/Test1.java
@@ -0,0 +1,18 @@
+package com.selenium.kk.maventest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","D:/Chrome/chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+	
+		driver.get("https:\\connect.maveric-systems.com");
+
+	}
+
+}
